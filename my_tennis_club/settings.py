d026584c0db232cdd1a7991acf32966eca2b3ffa
Sadullah <104268731+Sadu-sadullah@users.noreply.github.com>
2024-03-14 11:19:20 +0800

    Update settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'my_tennis_club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /'members/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
