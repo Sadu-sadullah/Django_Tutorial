03f42a681457c453159cf16b3d322379d65598cd
Sadullah <104268731+Sadu-sadullah@users.noreply.github.com>
2024-03-14 11:14:51 +0800

    Update settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+e2omuk_ple@r4!&4n%3b4s*uy2k*12pv&o)%xn9o_vp8!7447'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
